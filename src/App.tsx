be48b599bd73306875bf4367726c9ac654478d30
Yulia <iqeen94@gmail.com>
2023-05-08 13:29:00 +0300

    NX-4-Created navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-const App = () => (
-  <h1 className="text-3xl font-bold text-red-500 underline text-center">
-    Hello world!
-  </h1>
-);
+import Navbar from "./components/organisms/Navbar/Navbar";
+
+const App = () => <Navbar />;
 
 export default App;
---
+++ src/components/organisms/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+
+const Navbar: FC = () => (
+  <nav>
+    <ul className="flex justify-between items-center gap-5 font-medium text-white">
+      <li className="cursor-pointer hover:opacity-80">Головна</li>
+      <li className="cursor-pointer hover:opacity-80">Фільми</li>
+      <li className="cursor-pointer hover:opacity-80">Новинки й популярне</li>
+    </ul>
+  </nav>
+);
+
+export default Navbar;
