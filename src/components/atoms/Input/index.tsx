7185784363f80158283399efad9f43e3f16ffc96
Yulia <iqeen94@gmail.com>
2023-05-16 13:49:35 +0300

    bugfix/NX-11-Added a few variants of styles for input

--- src/components/atoms/Input/index.tsx
+++ src/components/atoms/Input/index.tsx
@@ -4,27 +4,44 @@ interface IInput {
   type: string;
   name: string;
   value?: string | number;
+  ref?: React.Ref<HTMLInputElement>;
   placeholder?: string;
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  className: string;
+  variant?: string;
 }
 
 const Input: FC<IInput> = ({
   type,
   name,
   value,
+  ref,
   placeholder,
   onChange,
-  className,
-}) => (
-  <input
-    type={type}
-    name={name}
-    value={value}
-    placeholder={placeholder}
-    onChange={onChange}
-    className={className}
-  />
-);
+  variant,
+}) => {
+  const searchInputStyle: string = "w-full h-full bg-transparent";
+  const hiddenInputStyle: string = "hidden";
+  const defaultInputStyle: string =
+    "w-full h-[50px] p-4 bg-neutral-850 border-b-2 rounded border-orange-550";
+
+  const inputVariant =
+    variant === "search"
+      ? searchInputStyle
+      : variant === "hidden"
+      ? hiddenInputStyle
+      : defaultInputStyle;
+
+  return (
+    <input
+      type={type}
+      name={name}
+      value={value}
+      placeholder={placeholder}
+      ref={ref}
+      onChange={onChange}
+      className={`text-white outline-none appearance-none ${inputVariant}`}
+    />
+  );
+};
 
 export default Input;
