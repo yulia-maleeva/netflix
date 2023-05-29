acf8a076de41015c8bb1e942b72ae323d91f5394
Yulia <iqeen94@gmail.com>
2023-05-29 12:09:37 +0300

    Reorganized Form Controllers folder structure and added redirect to Login form

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,5 +1,16 @@
+import { Control, FieldValues } from "react-hook-form";
+
 export interface IFormInputs {
   firstName: string;
   email: string;
   password: string;
+}
+
+export interface IController {
+  name: keyof IFormInputs;
+  control: Control<IFormInputs>;
+  rules?: FieldValues;
+  placeholder?: string;
+  variant?: "filled" | "transparent";
+  className?: string;
 }
