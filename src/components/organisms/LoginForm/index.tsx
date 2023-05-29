d6f96e681462c743fe8cf4fe23a0ab03fc06e093
Yulia <iqeen94@gmail.com>
2023-05-29 14:57:13 +0300

    Added temporary user verification and commented it, added link to sign up form, fixed some style errors

--- src/components/organisms/LoginForm/index.tsx
+++ src/components/organisms/LoginForm/index.tsx
@@ -1,19 +1,22 @@
 import React, { FC } from "react";
 
-import { useNavigate } from "react-router-dom";
-import ROUTES from "../../../constants/routes";
-
 import { useForm, SubmitHandler } from "react-hook-form";
 import { IFormInputs } from "../../../types";
 
+import { Link, useNavigate } from "react-router-dom";
+import ROUTES from "../../../constants/routes";
+
 import Button from "../../atoms/Button";
+
 import {
   EmailController,
   PasswordController,
 } from "../../molecules/FormControllers";
 
 const LoginForm: FC = () => {
-  const navigate = useNavigate();
+  //const isAuthorized = true;
+
+  //const navigate = useNavigate();
 
   const { handleSubmit, control, reset } = useForm<IFormInputs>({
     defaultValues: {
@@ -27,7 +30,11 @@ const LoginForm: FC = () => {
     console.log(data);
     reset();
 
-    navigate(`${ROUTES.HOME}`);
+    // if (isAuthorized) {
+    //   navigate(`${ROUTES.HOME}`);
+    // } else {
+    //   navigate(`${ROUTES.SIGN_UP}`);
+    // }
   };
 
   return (
@@ -38,7 +45,6 @@ const LoginForm: FC = () => {
       <h1 className="mb-5 text-[32px] font-medium text-white text-center">
         Login
       </h1>
-
       <EmailController
         name="email"
         control={control}
@@ -51,9 +57,18 @@ const LoginForm: FC = () => {
         variant="transparent"
         className="p-4 border border-white rounded"
       />
-      <Button variant="red" className="w-[200px] mt-5 self-center">
+      <Button type="submit" variant="red" className="w-50 my-5 self-center">
         Sign in
       </Button>
+      <p className="text-neutral-500 text-center">
+        First time at Netflix?&nbsp;
+        <Link
+          to={ROUTES.SIGN_UP}
+          className="text-white hover:underline decoration-white"
+        >
+          Sign up
+        </Link>
+      </p>
     </form>
   );
 };
