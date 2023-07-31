c75858f892c39a17639b8e5452a950283394c669
elonehoo <hi@elonehoo.me>
2023-07-31 20:36:20 +0800

    feat: add `vite-plugin-alias` config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import Alias from 'vite-plugin-alias'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "~": "/src",
-    },
-  },
+  plugins: [
+    react(),
+    Alias({
+      useConfig: true,
+      useTypescript: true,
+    })
+  ],
 });
